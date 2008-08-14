5bbcfdda5b429de5056534407b0309af836a9dd2
thisfred@gmail.com <>
2008-08-14 15:07:37 +0000

    testing the test methods :)

--- test_autoqueue.py
+++ test_autoqueue.py
@@ -137,3 +137,47 @@ class TestAutoQueue(object):
         row = self.autoqueue.get_track(artist, title)
         assert_equals((artist_id, title, None), row[1:])
 
+    def test_get_similar_artists(self):
+        """really only testing that we didn't break the test methods,
+        since we're overriding the real methods."""
+        artist = 'joni mitchell'
+        similar_artists = self.autoqueue.get_similar_artists(artist)
+        td = [(10000, u'carole king'), (9144, u'kate bush'),
+              (8834, u'rickie lee jones'), (7709, u'van morrison'),
+              (7459, u'crosby, stills, nash & young'), (7215, u'neil young'),
+              (7168, u'nick drake'), (7026, u'joan baez'),
+              (6958, u'james taylor'), (6695, u'paul simon'),
+              (6335, u'suzanne vega'), (6311, u'patti smith'),
+              (6281, u'the band'), (6151, u'bob dylan'),
+              (6065, u'crosby, stills & nash'), (6033, u'joan armatrading'),
+              (5978, u'marianne faithfull'), (5921, u'aimee mann'),
+              (5823, u'dusty springfield'), (5734, u'tim buckley'),
+              (5728, u'nina simone'), (5717, u'vashti bunyan'),
+              (5656, u'the byrds'), (5477, u'leonard cohen'),
+              (5264, u'rufus wainwright'), (5242, u'simon & garfunkel'),
+              (5224, u'cat power'), (5207, u'indigo girls'), (5195, u'nico'),
+              (5097, u'natalie merchant'), (5062, u'stevie wonder')]
+        assert_equals(td, similar_artists)
+
+    def test_get_similar_tracks(self):
+        """really only testing that we didn't break the test methods,
+        since we're overriding the real methods."""
+        artist = 'nina simone'
+        title = "i think it's going to rain today"
+        similar_tracks = self.autoqueue.get_similar_tracks(artist, title)
+        td = [(446, u'dinah washington', u'our love is here to stay'),
+              (444, u'dinah washington', u'love for sale'),
+              (443, u'marlena shaw', u'will i find my love today?'),
+              (438, u'minnie riperton', u'reasons'),
+              (436, u'minnie riperton', u'adventures in paradise'),
+              (428, u'cassandra wilson', u'find him'),
+              (426, u'al jarreau', u'jacaranda bougainvillea'),
+              (426, u'jimmy smith and wes montgomery', u'mellow mood'),
+              (421, u'alice russell', u"i'm just here"),
+              (419, u'aretha franklin', u'love for sale'),
+              (419, u'al jarreau', u'better than anything (live version)'),
+              (418, u'natalie williams', u'remember the day'),
+              (418, u'natalie williams', u'girlfriend'),
+              (415, u'sarah vaughan', u'i could write a book'),
+              (415, u'sarah vaughan', u'honeysuckle rose')]
+        assert_equals(td, similar_tracks)
