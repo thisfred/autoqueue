9b454cad94649e5d7022bd81daa0d4fc33291c11
thisfred@gmail.com <>
2008-08-18 18:38:36 +0000

    changed the order to test get_sorted, and removed the tests that didn't really test anything.

--- test_autoqueue.py
+++ test_autoqueue.py
@@ -21,13 +21,12 @@ class MyAutoQueue(AutoQueueBase):
     def get_similar_artists(self, artist_name):
         similar_artists = {
             'joni mitchell': [
-            (10000, u'carole king'), (9144, u'kate bush'),
-            (8834, u'rickie lee jones'), (7709, u'van morrison'),
             (7459, u'crosby, stills, nash & young'), (7215, u'neil young'),
             (7168, u'nick drake'), (7026, u'joan baez'),
             (6958, u'james taylor'), (6695, u'paul simon'),
             (6335, u'suzanne vega'), (6311, u'patti smith'),
             (6281, u'the band'), (6151, u'bob dylan'),
+            (8834, u'rickie lee jones'), (7709, u'van morrison'),
             (6065, u'crosby, stills & nash'), (6033, u'joan armatrading'),
             (5978, u'marianne faithfull'), (5921, u'aimee mann'),
             (5823, u'dusty springfield'), (5734, u'tim buckley'),
@@ -35,21 +34,22 @@ class MyAutoQueue(AutoQueueBase):
             (5656, u'the byrds'), (5477, u'leonard cohen'),
             (5264, u'rufus wainwright'), (5242, u'simon & garfunkel'),
             (5224, u'cat power'), (5207, u'indigo girls'), (5195, u'nico'),
+            (10000, u'carole king'), (9144, u'kate bush'),
             (5097, u'natalie merchant'), (5062, u'stevie wonder')],
             'nina simone': [
-            (10000, u'billie holiday'), (7934, u'ella fitzgerald'),
             (7402, u'sarah vaughan'), (6731, u'dinah washington'),
             (6518, u'madeleine peyroux'), (6042, u'etta james'),
             (5065, u'peggy lee'), (4984, u'julie london'),
             (4905, u'ella fitzgerald & louis armstrong'),
-            (4887, u'blossom dearie'), (4743, u'cassandra wilson'),
             (4651, u'aretha franklin'), (4488, u'bessie smith'),
             (4435, u'carmen mcrae'), (4380, u"anita o'day"),
             (4367, u'nat king cole'), (4311, u'lisa ekdahl'),
             (3896, u'diana krall'), (3738, u'louis armstrong'),
+            (4887, u'blossom dearie'), (4743, u'cassandra wilson'),
             (3385, u'dionne warwick'), (3243, u'corinne bailey rae'),
             (3206, u'mahalia jackson'), (3064, u'bill withers'),
             (2720, u'amy winehouse'), (2606, u'dusty springfield'),
+            (10000, u'billie holiday'), (7934, u'ella fitzgerald'),
             (2501, u'al green'), (2326, u'joni mitchell'),
             (2288, u'ray charles'), (2226, u'sam cooke')]}
         return similar_artists.get(artist_name)
@@ -59,33 +59,33 @@ class MyAutoQueue(AutoQueueBase):
             ('joni mitchell', 'carey'): 
             [(838, u'nick drake', u'things behind the sun'),
              (807, u'nick drake', u'horn'),
-             (715, u'joanna newsom', u'peach, plum, pear'),
-             (700, u'leonard cohen', u'suzanne'),
-             (691, u'joanna newsom', u'sprout and the bean'),
-             (664, u'bob dylan', u"blowin' in the wind"),
-             (635, u'leonard cohen', u'famous blue raincoat'),
              (598, u'simon & garfunkel', u'song for the asking'),
              (593, u'bob dylan', u"the times they are a-changin'"),
              (535, u'simon & garfunkel', u'keep the customer satisfied'),
              (520, u'cat stevens', u'peace train'),
+             (715, u'joanna newsom', u'peach, plum, pear'),
+             (700, u'leonard cohen', u'suzanne'),
+             (691, u'joanna newsom', u'sprout and the bean'),
+             (664, u'bob dylan', u"blowin' in the wind"),
              (511, u'james taylor', u'fire and rain'),
+             (635, u'leonard cohen', u'famous blue raincoat'),
              (451, u'james taylor', u'enough to be on your way'),
              (449, u'judee sill', u"that's the spirit"),
              (449, u'joan baez', u'asimbonanga')],
             ('nina simone', "i think it's going to rain today"):
             [(446, u'dinah washington', u'our love is here to stay'),
              (444, u'dinah washington', u'love for sale'),
-             (443, u'marlena shaw', u'will i find my love today?'),
-             (438, u'minnie riperton', u'reasons'),
-             (436, u'minnie riperton', u'adventures in paradise'),
-             (428, u'cassandra wilson', u'find him'),
              (426, u'al jarreau', u'jacaranda bougainvillea'),
              (426, u'jimmy smith and wes montgomery', u'mellow mood'),
-             (421, u'alice russell', u"i'm just here"),
+             (443, u'marlena shaw', u'will i find my love today?'),
+             (438, u'minnie riperton', u'reasons'),
              (419, u'aretha franklin', u'love for sale'),
              (419, u'al jarreau', u'better than anything (live version)'),
              (418, u'natalie williams', u'remember the day'),
              (418, u'natalie williams', u'girlfriend'),
+             (436, u'minnie riperton', u'adventures in paradise'),
+             (428, u'cassandra wilson', u'find him'),
+             (421, u'alice russell', u"i'm just here"),
              (415, u'sarah vaughan', u'i could write a book'),
              (415, u'sarah vaughan', u'honeysuckle rose'),]}
         return similar_tracks.get((artist_name, title))
@@ -136,51 +136,6 @@ class TestAutoQueue(object):
         artist_id = self.autoqueue.get_artist(artist)[0]
         row = self.autoqueue.get_track(artist, title)
         assert_equals((artist_id, title, None), row[1:])
-
-    def test_get_similar_artists(self):
-        """really only testing that we didn't break the test methods,
-        since we're overriding the real methods."""
-        artist = 'joni mitchell'
-        similar_artists = self.autoqueue.get_similar_artists(artist)
-        td = [(10000, u'carole king'), (9144, u'kate bush'),
-              (8834, u'rickie lee jones'), (7709, u'van morrison'),
-              (7459, u'crosby, stills, nash & young'), (7215, u'neil young'),
-              (7168, u'nick drake'), (7026, u'joan baez'),
-              (6958, u'james taylor'), (6695, u'paul simon'),
-              (6335, u'suzanne vega'), (6311, u'patti smith'),
-              (6281, u'the band'), (6151, u'bob dylan'),
-              (6065, u'crosby, stills & nash'), (6033, u'joan armatrading'),
-              (5978, u'marianne faithfull'), (5921, u'aimee mann'),
-              (5823, u'dusty springfield'), (5734, u'tim buckley'),
-              (5728, u'nina simone'), (5717, u'vashti bunyan'),
-              (5656, u'the byrds'), (5477, u'leonard cohen'),
-              (5264, u'rufus wainwright'), (5242, u'simon & garfunkel'),
-              (5224, u'cat power'), (5207, u'indigo girls'), (5195, u'nico'),
-              (5097, u'natalie merchant'), (5062, u'stevie wonder')]
-        assert_equals(td, similar_artists)
-
-    def test_get_similar_tracks(self):
-        """really only testing that we didn't break the test methods,
-        since we're overriding the real methods."""
-        artist = 'nina simone'
-        title = "i think it's going to rain today"
-        similar_tracks = self.autoqueue.get_similar_tracks(artist, title)
-        td = [(446, u'dinah washington', u'our love is here to stay'),
-              (444, u'dinah washington', u'love for sale'),
-              (443, u'marlena shaw', u'will i find my love today?'),
-              (438, u'minnie riperton', u'reasons'),
-              (436, u'minnie riperton', u'adventures in paradise'),
-              (428, u'cassandra wilson', u'find him'),
-              (426, u'al jarreau', u'jacaranda bougainvillea'),
-              (426, u'jimmy smith and wes montgomery', u'mellow mood'),
-              (421, u'alice russell', u"i'm just here"),
-              (419, u'aretha franklin', u'love for sale'),
-              (419, u'al jarreau', u'better than anything (live version)'),
-              (418, u'natalie williams', u'remember the day'),
-              (418, u'natalie williams', u'girlfriend'),
-              (415, u'sarah vaughan', u'i could write a book'),
-              (415, u'sarah vaughan', u'honeysuckle rose')]
-        assert_equals(td, similar_tracks)
 
     def test_get_sorted_similar_artists(self):
         artist = 'nina simone'
