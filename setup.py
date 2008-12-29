d61462145b94bf84d7238577b6f780d49860b94d
thisfred@gmail.com <>
2008-12-29 19:25:56 +0000

    restructuring

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name = "autoqueue",
     version = "0.3",
-    packages = find_packages(),
     package_data = {'': ['*.filter'],},
     # metadata for upload to PyPI
     author = "Eric Casteleijn",
@@ -14,5 +13,6 @@ setup(
     extras_require = {
     'mirage': ['ctypes', 'scipy'],
     },
-    include_package_data = True
+    package_dir = {'': 'src'},
+    include_package_data = True,
 )
