0f64c7ada81fdc8e5197d81176fa5e37fd5329c1
Eric Casteleijn <eric.casteleijn@canonical.com>
2011-05-03 11:19:43 -0400

    [release] 1.0.0alpha2

--- setup.py
+++ setup.py
@@ -73,13 +73,12 @@ if DATA_FILES:
 
 DistUtilsExtra.auto.setup(
     name='autoqueue',
-    version='1.0alpha1',
+    version='1.0alpha2',
     packages=['autoqueue', 'mirage'],
     license='GNU GPL v2',
     author='Eric Casteleijn',
     author_email='thisfred@gmail.com',
     description='A cross music player plugin that queues similar tracks.',
-    long_description=open('README.txt').read(),
     url='https://launchpad.net/autoqueue',
     package_data={'mirage': ['res/*']},
     requires=['scipy', 'ctypes'],
