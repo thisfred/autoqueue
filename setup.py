05b01630f9b09a629cb3cfb7a5637565a181f08a
Eric Casteleijn <eric.casteleijn@canonical.com>
2011-05-06 13:57:54 -0400

    [release] 1.0.0-alpha7

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ if DATA_FILES:
 DistUtilsExtra.auto.setup(
     name='autoqueue',
     description='A cross music player plugin that queues similar tracks',
-    version='1.0.0~ppa7',
+    version='1.0.0-alpha7',
     packages=['autoqueue', 'mirage'],
     license='GNU GPL v2',
     author='Eric Casteleijn',
