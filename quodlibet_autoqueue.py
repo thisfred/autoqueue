0dfbc1e881e2bfd4dbedb84cf6a542a224c1bd71
Eric Casteleijn <eric.casteleijn@canonical.com>
2011-05-13 11:43:15 +0200

    - fixed docstrings - catch weird errors in playcount and skipcount

--- quodlibet_autoqueue.py
+++ quodlibet_autoqueue.py
@@ -6,7 +6,8 @@ This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 2 as
 published by the Free Software Foundation"""
 
-import gtk, gobject
+import gtk
+import gobject
 from gtk import gdk
 from plugins.events import EventPlugin
 from widgets import main
@@ -16,10 +17,6 @@ import config
 from collections import deque
 
 from autoqueue import AutoQueueBase, SongBase
-
-from mirage import Mir
-
-import gst
 
 INT_SETTINGS = {
     'artist_block_time': {
@@ -48,11 +45,12 @@ NO_OP = lambda *a, **kw: None
 
 
 def escape(the_string):
-    """double escape quotes"""
+    """Double escape quotes."""
     return the_string.replace('"', '\\"')
 
 
 def remove_role(artist):
+    """Remove performer role from string."""
     if not artist.endswith(')'):
         return artist
     return artist.split('(')[0]
@@ -70,7 +68,7 @@ class Song(SongBase):
         performers = [remove_role(artist.lower()) for artist in self.song.list(
             "performer")]
         if hasattr(self.song, '_song'):
-            for tag in self.song._song:
+            for tag in self.song._song:  # pylint: disable=W0212
                 if tag.startswith('performer:'):
                     performers.extend(
                         [artist.lower() for artist in self.song.list(tag)])
@@ -91,26 +89,41 @@ class Song(SongBase):
         return title
 
     def get_tags(self):
-        """return a list of tags for the songs"""
+        """Get a list of tags for the song."""
         return self.song.list("grouping")
 
     def get_filename(self):
-        """return a list of tags for the songs"""
+        """Get the filename of the song."""
         return self.song("~filename")
 
     def get_length(self):
+        """Get the length in seconds of the song."""
         return self.song("~#length")
 
     def get_playcount(self):
-        return self.song("~#playcount") + self.song('~#skipcount')
+        """Get the total playcount for the song."""
+        try:
+            playcount = int(self.song("~#playcount"))
+        except ValueError:
+            # XXX: WTF: playcount can be an empty string??
+            playcount = 0
+        try:
+            skipcount = int(self.song('~#skipcount'))
+        except ValueError:
+            # XXX: WTF: skipcount can be an empty string??
+            skipcount = 0
+        return playcount + skipcount
 
     def get_added(self):
+        """Get the date the song was added to the library."""
         return self.song("~#added")
 
     def get_last_started(self):
+        """Get the datetime the song was last started."""
         return self.song("~#laststarted")
 
     def get_rating(self):
+        """Get the rating for the song."""
         return self.song("~#rating")
 
 
@@ -126,13 +139,6 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         EventPlugin.__init__(self)
         AutoQueueBase.__init__(self)
         self._generators = deque()
-
-    @property
-    def mir(self):
-        if hasattr(main, 'mir'):
-            return main.mir
-        main.mir = Mir()
-        return main.mir
 
     def enabled(self):
         """user enabled the plugin"""
@@ -153,13 +159,17 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         self.on_song_started(ssong)
 
     def plugin_on_removed(self, songs):
+        """Handle song(s) removed event."""
         for song in songs:
             self.similarity.remove_track_by_filename(
                 song('~filename'), reply_handler=NO_OP,
                 error_handler=self.error_handler)
 
-    def PluginPreferences(self, parent):
+    def PluginPreferences(self, parent):  # pylint: disable=C0103
+        """Set and unset preferences from gui or config file."""
+
         def bool_changed(widget):
+            """Boolean setting changed."""
             if widget.get_active():
                 setattr(self, widget.get_name(), True)
             else:
@@ -170,11 +180,13 @@ class AutoQueue(EventPlugin, AutoQueueBase):
                 widget.get_active() and 'true' or 'false')
 
         def str_changed(entry, key):
+            """String setting changed."""
             value = entry.get_text()
             config.set('plugins', 'autoqueue_%s' % key, value)
             setattr(self, key, value)
 
         def int_changed(entry, key):
+            """Integer setting changed."""
             value = entry.get_text()
             if value:
                 config.set('plugins', 'autoqueue_%s' % key, value)
@@ -224,6 +236,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return table
 
     def _idle_callback(self):
+        """Idle callback for async processing."""
         gdk.threads_enter()
         while self._generators:
             for dummy in self._generators[0]:
@@ -248,7 +261,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
             gobject.idle_add(self._idle_callback)
 
     def player_construct_file_search(self, filename, restrictions=None):
-        """construct a search that looks for songs with this filename"""
+        """Construct a search that looks for songs with this filename."""
         if not filename:
             return
         search = '~filename="%s"' % (escape(filename),)
@@ -257,8 +270,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return search
 
     def player_construct_track_search(self, artist, title, restrictions=None):
-        """construct a search that looks for songs with this artist
-        and title"""
+        """Construct an artist and title search."""
         search = '&(artist = "%s", title = "%s", version="")' % (
             escape(artist), escape(title))
         if "(" in title:
@@ -281,8 +293,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return search
 
     def player_construct_tag_search(self, tags, restrictions=None):
-        """construct a search that looks for songs with these
-        tags"""
+        """Construct a tags search."""
         search = ''
         search_tags = []
         for tag in tags:
@@ -297,7 +308,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return search
 
     def player_construct_artist_search(self, artist, restrictions=None):
-        """construct a search that looks for songs with this artist"""
+        """Construct a search that looks for songs with this artist."""
         search = '|(artist = "%s",performer="%s")' % (
             escape(artist), escape(artist))
         if restrictions:
@@ -305,7 +316,7 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return search
 
     def player_set_variables_from_config(self):
-        """Initialize user settings from the configuration storage"""
+        """Initialize user settings from the configuration storage."""
         for key, vdict in INT_SETTINGS.items():
             try:
                 setattr(self, key, config.getint(
@@ -330,15 +341,15 @@ class AutoQueue(EventPlugin, AutoQueueBase):
                 config.set("plugins", "autoqueue_%s" % key, vdict['value'])
 
     def player_get_queue_length(self):
-        """Get the current length of the queue"""
+        """Get the current length of the queue."""
         return sum([row.get("~#length", 0) for row in main.playlist.q.get()])
 
     def player_enqueue(self, song):
-        """Put the song at the end of the queue"""
+        """Put the song at the end of the queue."""
         main.playlist.enqueue([song.song])
 
     def player_search(self, search):
-        """perform a player search"""
+        """Perform a player search."""
         try:
             myfilter = Query(search).search
             songs = filter(myfilter, library.itervalues())
@@ -348,5 +359,5 @@ class AutoQueue(EventPlugin, AutoQueueBase):
         return [Song(song) for song in songs]
 
     def player_get_songs_in_queue(self):
-        """return (wrapped) song objects for the songs in the queue"""
+        """Return (wrapped) song objects for the songs in the queue."""
         return [Song(song) for song in main.playlist.q.get()]
