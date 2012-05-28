4b83638cc626109e7087b6b3abe45101f8fd8f9d
Eric Casteleijn <eric.casteleijn@canonical.com>
2012-05-28 10:32:29 -0400

    make xdg softer dependency, use /usr/bin/env python in script.

--- autoqueue/similarity.py
+++ autoqueue/similarity.py
@@ -277,8 +277,10 @@ class Similarity(object):
 
         """
         if not XDG:
-            return NotImplemented
-        data_dir = os.path.join(xdg.BaseDirectory.xdg_data_home, 'autoqueue')
+            data_dir = os.path.join(os.path.expanduser('~'), '.autoqueue')
+        else:
+            data_dir = os.path.join(
+                xdg.BaseDirectory.xdg_data_home, 'autoqueue')
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         return data_dir
